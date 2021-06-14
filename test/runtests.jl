cbb5d2d22a1a700050297cee49b23538b84f2b64
Conall Timoney <conalltimoney@gmail.com>
2021-06-14 20:14:02 +0100

    added tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,250 @@
 using Test
+using DynamicPipes
 
-@test 1 == 1 
+# basic tests 
+@testset "@>, @>> pipeing into one function" begin
+    x = [1,2,3]
+    
+    y = x |>
+        @>  sum
+    @test y == sum(x)
 
-@test
+    y = x |>
+        @> sum()
+    @test y == sum(x)
+        
+    y = x |>
+        @> sum(_)
+    @test y == sum(x)
+    
+   y = @>> x |> sum
+   @test y == sum(x)
+   
+   y = @>> x |> sum()
+   @test y == sum(x)
+   
+   y = @>> x |> sum(_)
+   @test y == sum(x)
+    
+end
+
+macro double(x)
+    return :($(esc(:(x + x))))
+end
+
+@testset "@>, @>> test pipeing into macros" begin
+    x = [1,2,3]
+    y = x + x
+
+    t1 = x |>
+        @>  @double()
+    t2 = x |>
+        @>  @double
+    t3 = x |> 
+        @>  @double(_) 
+        
+    @test y == t1
+    @test y == t2
+    @test y == t3
+    
+    t1 = @>> x |> @double
+    t2 = @>> x |> @double()
+    t3 = @>> x |> @double(_)
+    
+    @test y == t1
+    @test y == t2
+    @test y == t3
+end
+
+@testset "@>, @>> test first arguemnt imputation" begin
+    x = [1, 2, 3]
+    
+    y = copy(x) |> 
+        @>  append!(4)
+    @test y == [1,2,3,4]
+    
+    y = @>> copy(x) |>
+        append!(4)
+    @test y == [1, 2, 3, 4]
+end
+
+@testset "@>, @>> test multiple pipes" begin
+    x = [1, 2, 3]
+    
+    y = x |> 
+        @>  copy |>
+            append!(4)
+    @test y == [1,2,3,4]
+    
+    y = @>> x |>
+        copy |> 
+        append!(4)
+        
+    @test y == [1, 2, 3, 4]
+end
+
+@testset "@>, @>> test vectorisation" begin
+    x = [1,2,3]
+    
+    y = x |>
+        @>  copy() |>
+            .+(1)
+    @test y == [2,3,4]
+    
+    y = @>> x |>
+        copy() |>
+        .+(1)
+    
+    @test y == [2,3,4]
+
+    x = [1,2,3]
+    
+    y = x |>
+        @>  copy .|>
+            +(1)
+    
+    @test y == [2,3,4]
+    
+    y = @>> x |>
+        copy .|>
+        +(1) 
+        
+    @test y == [2,3,4]
+         
+end
+
+@testset "@>, @>> test pipes in pipes" begin
+    x = [1,2,3]
+    z = [1,1,1]
+    
+    y = x |> 
+        @>  +(x |> 
+                @>  _ .+ 1 |>
+                    _ .* 2) |>
+            _ + z |>
+            .-(1)
+
+    @test y == (
+        (x + ((x .+ 1) .* 2)) + z .- 1 
+    ) 
+    
+    y = @>> x |> 
+        +(x |> 
+            @>  _ .+ 1 |>
+                _ .* 2) |>
+        _ + z |>
+        .-(1)
+        
+    @test y == (
+        (x + ((x .+ 1) .* 2)) + z .- 1 
+    ) 
+    
+    y = @>> x |> 
+        +(@>> x |> 
+            _ .+ 1 |>
+            _ .* 2) |>
+        _ + z |>
+        .-(1)
+        
+    @test y == (
+        (x + ((x .+ 1) .* 2)) + z .- 1 
+    ) 
+    
+    y = @>> x |> 
+        +(@>> _ |> 
+            _ .+ 1 |>
+            _ .* 2) |>
+        _ + z |>
+        .-(1)
+        
+    @test y == (
+        (((x .+ 1) .* 2) + z) .- 1  
+    )
+    
+    y = @>> x |> 
+        _ + @>> _ |> 
+            _ .+ 1 |>
+            _ .* 2 |>
+        _ + z |>
+        .-(1)
+        
+    @test y == (
+        (x + ((x .+ 1) .* 2) + z) .- 1  
+    )
+    
+    y = @>> x |> 
+            +(_ |> 
+                @>  _ .+ 1 |>
+                    _ .* 2) |>
+            _ + z |>
+            .-(1)
+        
+    @test y == (
+        (((x .+ 1) .* 2) + z) .- 1  
+    )
+    
+    y = x |> 
+        @>  +(_ |> 
+                @>  _ .+ 1 |>
+                    _ .* 2) |>
+            _ + z |>
+            .-(1)
+        
+    @test y == (
+        (((x .+ 1) .* 2) + z) .- 1  
+    )
+    
+    y = x |> 
+        @>  _ + (_ |> 
+                    @>  _ .+ 1 |>
+                        _ .* 2) |>
+            _ + z |>
+            .-(1)
+        
+    @test y == (
+        (x + ((x .+ 1) .* 2) + z) .- 1  
+    )
+end
+
+@testset "@>, @>> test block input" begin
+    x = [1,2,3]
+    z = [1,1,1]
+    
+    y = x |> 
+        @> begin 
+            +(x |> 
+                @>  _ .+ 1 |>
+                    _ .* 2) 
+            _ + z 
+            .-(1)
+        end
+
+    @test y == (
+        (x + ((x .+ 1) .* 2)) + z .- 1 
+    )
+    
+    y = @>> begin 
+            x 
+            +(x |> 
+                @>  _ .+ 1 |>
+                    _ .* 2) 
+            _ + z 
+            .-(1)
+        end
+
+    @test y == (
+        (x + ((x .+ 1) .* 2)) + z .- 1 
+    )
+    
+    y = @>> x begin 
+            +(x |> 
+                @>  _ .+ 1 |>
+                    _ .* 2) 
+            _ + z 
+            .-(1)
+        end
+
+    @test y == (
+        (x + ((x .+ 1) .* 2)) + z .- 1 
+    )
+end
