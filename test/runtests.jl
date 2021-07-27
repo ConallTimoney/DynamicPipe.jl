5f850aa025f1d50e5666fa070b62633b9670e56e
Conall Timoney <conalltimoney@gmail.com>
2021-07-27 21:36:18 +0100

    added new tests for pipes in pipes functionality

--- test/runtests.jl
+++ test/runtests.jl
@@ -29,7 +29,7 @@ using DynamicPipe
 end
 
 macro double(x)
-    return :($(esc(:(x + x))))
+    return :($(esc(:($x + $x))))
 end
 
 @testset "@>, @>> test pipeing into macros" begin
@@ -56,7 +56,7 @@ end
     @test y == t3
 end
 
-@testset "@>, @>> test first arguemnt imputation" begin
+@testset "@>, @>> test first argument imputation" begin
     x = [1, 2, 3]
     
     y = copy(x) |> 
@@ -120,6 +120,11 @@ end
         (x + ((x .+ 1) .* 2)) + z .- 1 
     ) 
     
+    # TODO fix
+    # make 2 versions of create create_anonymous_function 
+    # one that also acts on second part and pipe func 
+    # problem is this :(x |> @> _  .+ 1 |> _ .* 2).args
+    # we are creating a function for the macro second part
     y = x |> 
         @>  +(x |> 
                 @>  _ .+ 1 |>
@@ -317,4 +322,158 @@ end
     end
     
     @test y == y_ans
+end
+
+@testset "@>, @>> test pipes in pipes without macros calls" begin
+    x = [1,2,3]
+    z = [1,1,1]
+    y_ans = (
+        (x + ((x .+ 1) .* 2)) + z .- 1 
+    ) 
+    
+    y = x |> 
+        @> +(x |>
+                _ .+ 1 |>
+                _ .* 2) |>
+            _ + z |>
+            .-(1)
+
+    @test y == y_ans
+    
+    y = @>> x |> 
+        +(x |> 
+            _ .+ 1 |>
+            _ .* 2) |>
+        _ + z |>
+        .-(1)
+        
+    @test y == y_ans
+    
+    
+    y = @>> x |> 
+        +(_ |> 
+            _ .+ 1 |>
+            _ .* 2) |>
+        _ + z |>
+        .-(1)
+        
+    @test y == (
+        (((x .+ 1) .* 2) + z) .- 1  
+    )
+    
+    y = @>> x |> 
+        _ + (_ |> 
+            _ .+ 1 |>
+            _ .* 2 |>
+        _ + z |>
+        .-(1))
+        
+    @test y == y_ans
+    
+    y = @>> x |> 
+            +(_ |> 
+                _ .+ 1 |>
+                _ .* 2) |>
+            _ + z |>
+            .-(1)
+        
+    @test y == (
+        (((x .+ 1) .* 2) + z) .- 1  
+    )
+    
+    y = x |> 
+        @>  +(_ |> 
+                _ .+ 1 |>
+                _ .* 2) |>
+            _ + z |>
+            .-(1)
+        
+    @test y == (
+        (((x .+ 1) .* 2) + z) .- 1  
+    )
+      
+
+    x = [1,2,3]
+    z = [1,1,1]
+    y_ans = (
+        (x + ((x .+ 1) .* 2)) + z .- 1 
+    )
+    
+    y = x |> 
+        @> begin 
+            +(x |> 
+                _ .+ 1 |>
+                _ .* 2) 
+            _ + z 
+            .-(1)
+        end
+
+    @test y == y_ans
+    
+    
+    y = @>> begin 
+            x 
+            +(x |> 
+                _ .+ 1 |>
+                _ .* 2) 
+            _ + z 
+            .-(1)
+        end
+
+    @test y == y_ans
+    
+    y = @>> begin 
+            x 
+            +(_, _ |> 
+                _ .+ 1 |>
+                _ .* 2) 
+            _ + z 
+            .-(1)
+        end
+
+    @test y == y_ans
+    
+    y = @>> x begin 
+            +(x |> 
+                _ .+ 1 |>
+                _ .* 2) 
+            _ + z 
+            .-(1)
+        end
+
+    @test y == y_ans
+    
+    y = @>> x begin 
+            +(_, _ |> 
+                _ .+ 1 |>
+                _ .* 2) 
+            _ + z 
+            .-(1)
+        end
+
+    @test y == y_ans
+
+
+    y = x |>
+        @>  _ + (_ .|> (_ + 1)) |>
+            (_ .+ sum((_ |> .*(3)))) |>
+            _ .+ (2 |> -(3)) 
+            
+            
+            
+    @test y == (x + (x .+ 1)) .+ sum((x + (x .+ 1)) .* 3) .+ (-1)
+     
+    
+    y = x |>
+        @>  _ .+ (2 |> -(3 |> +(6))) .|>
+            _ + (7 |> -(1 |> +(2))) |>
+            _ .- 2 .|>
+            +(1)         
+    
+    @test y == (
+        x .+ (2 - 9) |> 
+        y -> y .+ (7 -3 ) |> 
+        y -> y .- 2 |> 
+        y -> y .+ 1
+    )
 end
